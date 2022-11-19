49fdf2e92537f360c8c0908fafec9906d5a3dac9
unexpectedhaki <thomas.pritchard20@gmail.com>
2022-11-19 12:15:10 -0500

    Javascript: created list items to display inputs as links that open in a new tab. Added a clear value function for the click event listener on the save input button. CSS: styled links to match the app theme.

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 let myLeads = []
-
-const inputEl = document.querySelector("#input-el")
-const inputBtn = document.querySelector("#input-btn")
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click",function() {
-    console.log("clicked")
     myLeads.push(inputEl.value)
     console.log(myLeads)
+    renderLeads()
+    inputEl.value = ""
 })
+
+function renderLeads() {
+    let listItems = ""
+
+    for (let i = 0; i < myLeads.length; i++) {
+        //listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                ${myLeads[i]}
+                </a>
+            </li>
+        `
+        console.log(listItems)
+    }
+
+    ulEl.innerHTML = listItems
+}
