9db9e351b314824af134beba64e8a5cdbfe99906
unexpectedhaki <thomas.pritchard20@gmail.com>
2022-11-21 22:09:11 -0500

    Added a variable to access localStorage values

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads") )
+
 inputBtn.addEventListener("click",function() {
     myLeads.push(inputEl.value)
-    console.log(myLeads)
-    renderLeads()
     inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    renderLeads()
 })
 
 function renderLeads() {
