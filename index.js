ba1bf7d7e39f7475d464038580875932a530bac0
unexpectedhaki <thomas.pritchard20@gmail.com>
2022-11-29 17:55:38 -0500

    Button Additions
    Created new buttons that include a save current tab feature and a delete all feature.
    Reorganized javascript code.
    Added arguments to the render function.

--- index.js
+++ index.js
@@ -2,30 +2,58 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const tabBtn = document.getElementById("tab-btn")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads") )
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads") )
+const tabs = [
+    {url: "https://www.linkedin.com/in/thomas-pritchard-a37374161/"}
+]
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+function render(leads) {
+    let listItems = ""
+
+    for (let i = 0; i < leads.length; i++) {
+        //listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                ${leads[i]}
+                </a>
+            </li>
+        `
+    }
+
+    ulEl.innerHTML = listItems
+}
 
 inputBtn.addEventListener("click",function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    renderLeads()
+    render(myLeads)
 })
 
-function renderLeads() {
-    let listItems = ""
+tabBtn.addEventListener("click", function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        console.log(tabs)
+        //since only one tab should be active and in the current window, the return variable should only have one entry
+        let activeTab = tabs[0]
+        let activeTabId = activeTab.id
+    })
 
-    for (let i = 0; i < myLeads.length; i++) {
-        //listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
-        listItems += `
-            <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                ${myLeads[i]}
-                </a>
-            </li>
-        `
-        console.log(listItems)
-    }
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
+})
 
-    ulEl.innerHTML = listItems
-}
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
